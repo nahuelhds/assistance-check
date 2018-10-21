08cd2f2586319437dcc211f30f113074f4b12118
Nahue <nsotelo@cristalmedia.net>
2018-10-21 20:23:51 -0300

    Notification

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Card, CardText, CardTitle } from "@material-ui/core/Card";
-import { TextField, Snackbar } from "@material-ui/core";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import { TextField, Snackbar, Typography } from "@material-ui/core";
 import FirebaseMessaging from "../services/FirebaseMessaging";
 
 class Notification extends React.Component {
@@ -42,27 +43,30 @@ class Notification extends React.Component {
 
   render() {
     return (
-      <div>
-        <Card>
-          <CardTitle title="Message" subtitle="Here is token for the browser" />
-          <CardText>
-            <TextField
-              floatingLabelText="Token"
-              disabled={true}
-              fullWidth={true}
-              multiLine={true}
-              rows={2}
-              value={this.state.token}
-            />
-          </CardText>
-        </Card>
-        <Snackbar
-          open={this.state.toast}
-          message={this.state.toastMessage}
-          autoHideDuration={4000}
-          onRequestClose={() => this.setState({ toast: false })}
-        />
-      </div>
+      <Card>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Message
+          </Typography>
+          <Typography variant="subtitle1" component="h5">
+            Here is token for the browser
+          </Typography>
+          <TextField
+            label="Token"
+            disabled={true}
+            fullWidth={true}
+            multiline
+            rows={2}
+            value={this.state.token}
+          />
+          <Snackbar
+            open={this.state.toast}
+            message={this.state.toastMessage}
+            autoHideDuration={4000}
+            onRequestClose={() => this.setState({ toast: false })}
+          />
+        </CardContent>
+      </Card>
     );
   }
 }
